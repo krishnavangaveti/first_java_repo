3665cfc1c0ec271b7ab0b2b6a6a720596231ceb0
Krishna Vangaveti <krishna.vangaveti@gmail.com>
2022-06-19 21:18:07 -0600

    added Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void message()
+	{
+		System.out.println("message fun called");
+	}
+	public static void main(String[] args)
+	{
+	System.out.println("Hai");
+	message();
+	}
+}
